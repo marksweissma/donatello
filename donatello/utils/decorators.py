fd190c2f6a95ca4c6b6e0a403a79cf6568acc5c4
Mark Weiss <mark.weiss.mof@gmail.com>
2018-10-14 20:04:26 -0700

    .

--- donatello/utils/decorators.py
+++ donatello/utils/decorators.py
@@ -79,46 +79,62 @@ def pandas_df(wrapped, instance, args, kwargs):
     return result
 
 
-@decorator
-#@fallback()
-def grid_search(wrapped, instance, args, kwargs):
-    """
-    Grid search of hyperparameter space
-
-    Instantates search through `obj.GridType` constructor call
-
-    Stores `gridSearch` attribute with object
-    """
-    X = args[0]
-    y = kwargs.pop('y', None)
-
-    gridSearch = kwargs.pop('gridSearch', None)
-    paramGrid = kwargs.pop('paramGrid', None)
-    gridKwargs = kwargs.pop('gridKwargs', None)
-
-    paramGrid = nvl(paramGrid, instance.paramGrid)
-    gridKwargs = nvl(gridKwargs, instance.gridKwargs)
-    print 'pre search'
-    print gridSearch
-    print paramGrid
-
-    if gridSearch and paramGrid:
-        from sklearn.model_selection import GridSearchCV
-        instance.gridSearch = GridSearchCV(estimator=instance,
-                                           param_grid=paramGrid,
-                                           **gridKwargs)
-        instance.gridSearch.fit(X, y=y, gridSearch=False)
-        instance.set_params(**instance.gridSearch.best_params_)
-
-    print 'post search'
-    result = wrapped(X, y=y, **kwargs)
-    return result
-
-
 # @decorator
-# def fallback(method):
-    # @wraps(method)
-    # def wrapped(self, *args, **kwargs):
-        # result = method(self, *args, **kwargs)
-        # return result
-    # return wrapped
+#@fallback()
+# def grid_search(wrapped, instance, args, kwargs):
+    # """
+    # Grid search of hyperparameter space
+
+    # Instantates search through `obj.GridType` constructor call
+
+    # Stores `gridSearch` attribute with object
+    # """
+    # X = args[0]
+    # y = kwargs.pop('y', None)
+
+    # gridSearch = kwargs.pop('gridSearch', None)
+    # paramGrid = kwargs.pop('paramGrid', None)
+    # gridKwargs = kwargs.pop('gridKwargs', None)
+
+    # paramGrid = nvl(paramGrid, instance.paramGrid)
+    # gridKwargs = nvl(gridKwargs, instance.gridKwargs)
+    # print 'pre search'
+    # print gridSearch
+    # print paramGrid
+
+    # if gridSearch and paramGrid:
+        # from sklearn.model_selection import GridSearchCV
+        # instance.gridSearch = GridSearchCV(estimator=instance,
+                                           # param_grid=paramGrid,
+                                           # **gridKwargs)
+        # instance.gridSearch.fit(X, y=y, gridSearch=False)
+        # instance.set_params(**instance.gridSearch.best_params_)
+
+    # print 'post search'
+    # result = wrapped(X, y=y, **kwargs)
+    # return result
+
+def coelesce(**defaults):
+    """
+    Key value pairs to enforce null value logic
+    """
+    @decorator
+    def _wrapper(wrapped, instance, args, kwargs):
+        for key, default in defaults.items():
+            kwargs[key] = nvl(kwargs.get(key, None), default)
+        result = wrapped(*args, **kwargs)
+        return result
+    return _wrapper
+
+
+def fallback(*defaults):
+    """
+    Keyword arguemts of attribute to fallback to of object
+    """
+    @decorator
+    def _wrapper(wrapped, instance, args, kwargs):
+        for default in defaults:
+            kwargs[default] = nvl(kwargs.get(default, None), getattr(instance, default, None))
+        result = wrapped(*args, **kwargs)
+        return result
+    return _wrapper
