7994abcfba6da3ec768d08847221dbfd2311c987
Mark Weiss <mark.weiss.mof@gmail.com>
2018-12-03 01:28:46 -0800

    bumb version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='donatello',
-      version='0.0.1',
+      version='0.0.4',
       author='Mark Weiss',
       author_email='mark.s.weiss.ma@gmail.com',
       url='git@github.com:marksweissma/donatello',
