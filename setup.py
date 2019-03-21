32a5067e89b30f80ca0f0d91cb00f42f57dc85f6
Mark Weiss <mark.weiss.mof@gmail.com>
2019-03-20 18:08:59 -0700

    lint setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='donatello',
-      version='0.0.4',
+      version='0.0.1',
       author='Mark Weiss',
       author_email='mark.s.weiss.ma@gmail.com',
-      url='git@github.com:marksweissma/donatello',
-      packages=find_packages()
+      url='git@github.com/marksweissma/donatello',
+      packages=find_packages('donatello')
       )
