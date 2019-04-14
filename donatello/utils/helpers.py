6269133ae3deb89bef401109e0016793709822e8
Mark Weiss <mark.weiss.mof@gmail.com>
2019-04-13 19:15:18 -0700

    test fest

--- donatello/utils/helpers.py
+++ donatello/utils/helpers.py
@@ -30,56 +30,7 @@ def nvl(*args):
     return args[-1]
 
 
-def has_nested_attribute(obj, attrPath, separator='_'):
-    """
-    Check whether nested attribute exists via string name
-
-    Args:
-        obj (object): Object to traverse.
-        attrPath (str): Path to attribute location split by separator,\
-            use empty string "" to return obj
-        separator (str): separator for nesting levels in string representation
-    """
-    if hasattr(obj, attrPath):
-        return True
-    elif not attrPath:
-        return obj is not None
-    else:
-        nextLevel, tail = attrPath.split(separator, 1)
-        return _get_nested_attribute(getattr(obj, nextLevel, None),
-                                     tail, separator)
-
-
-def _get_nested_attribute(obj, attrPath, separator='_'):
-    if attrPath == "":
-        return obj
-    elif hasattr(obj, attrPath):
-        return getattr(obj, attrPath)
-    else:
-        nextLevel, tail = attrPath.split(separator, 1)
-        return _get_nested_attribute(getattr(obj, nextLevel, None),
-                                     tail, separator)
-
-
-def get_nested_attribute(obj, attrPath, separator='_'):
-    """
-    Get nested attribute via string name. passing empty string returns obj
-
-    Args:
-        obj (object): Object to traverse.
-        attrPath (str): Path to attribute location split by separator,\
-            use empty string "" to return obj
-        separator (str): separator for nesting levels in string representation
-    """
-    if attrPath and not has_nested_attribute(obj, attrPath, separator):
-        raise AttributeError('{obj} does not have {attrPath}'.format(
-                             obj=obj, attrPath=attrPath)
-                             )
-    else:
-        return _get_nested_attribute(obj, attrPath, separator)
-
-
-# dispatch on shape make registers :/
+# dispatch on shape make registers
 def reformat_aggs(d, idx=0, sortValues=None, indexName=None, filterNulls=.95):
     information = [pd.Series(value[value.columns[idx]], name=key) for key, value in d.items()]
     df = pd.concat(information, axis=1, sort=True)
---
+++ tests/test_helpers.py
@@ -0,0 +1,77 @@
+import pytest
+import pandas as pd
+from donatello.utils import helpers
+
+
+@pytest.fixture
+def lst():
+    return []
+
+
+@pytest.fixture
+def e():
+    return pd.np.array([])
+
+
+@pytest.fixture
+def a():
+    return pd.np.array(['a'])
+
+
+@pytest.fixture
+def ab():
+    return pd.np.array(['a', 'b'])
+
+
+class O(object):
+    a = 1
+
+    def __init__(self, b=2):
+        self.b = b
+
+    @property
+    def o(self):
+        return O()
+
+
+@pytest.fixture
+def o():
+    return O()
+
+
+def test_nvl(lst, e, a, ab):
+    assert lst == helpers.nvl(None, lst)
+    assert None is helpers.nvl(lst, None)
+
+    assert a == helpers.nvl(None, a)
+    assert a == helpers.nvl(a, None)
+
+    assert (ab == helpers.nvl([], ab)).all()
+    assert (ab == helpers.nvl(ab, [])).all()
+
+
+def test_has(o):
+    assert helpers.has(o, 'a', ())
+    assert helpers.has({'a': None}, 'a', slicers=(dict,))
+    assert not helpers.has(o, 'c', ())
+    assert not helpers.has({'a': None}, 'c', slicers=(dict,))
+
+
+def test_get(o):
+    assert helpers.get(o, 'a', (), 'ignore', None) == 1
+    assert helpers.get({'a': 1}, 'a', (dict,), 'ignore', None) == 1
+
+    assert helpers.get(o, 'c', (), 'ignore', None) is None
+    assert helpers.get({'a': None}, 'c', (dict,), 'ignore', None) is None
+
+    with pytest.raises(AttributeError):
+        helpers.get(o, 'c', (), 'raise', None)
+
+    with pytest.raises(KeyError):
+        helpers.get({'a': None}, 'c', (dict,), 'raise', None)
+
+
+def test_access(o):
+    assert helpers.access(o, []) is o
+    assert helpers.access(o, ['']) is o
+    assert helpers.access(o, ['a']) == 1
